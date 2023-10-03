fd6be643a89266acb8c8ed3271e33d2cab5e3f89
Max Chapman <max.chapman@cloudhouse.com>
2023-10-04 00:05:11 +0100

    Finish off basics of API

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,24 @@
-const express = require('express');
-require('dotenv').config('.env');
+const express = require("express");
+require("dotenv").config(".env");
 
-const { MongoClient } = require('mongodb');
-const url = process.env.MONGO_URL
+const { MongoClient } = require("mongodb");
+const url = process.env.MONGO_URL;
 
 const client = new MongoClient(url);
-const db = client.db("data")
-export const col = db.collection("objects") 
+const db = client.db("data");
+export const col = db.collection("objects");
 
 import morgan from "morgan";
 import helmet from "helmet";
 import cors from "cors";
 
 import * as middlewares from "./middlewares";
-import { postSurvey } from "./controllers/surveys";
+import {
+  postSurvey,
+  getSurveys,
+  getSurvey,
+  putSurvey,
+} from "./controllers/surveys";
 
 require("dotenv").config();
 
@@ -24,7 +29,10 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get("/", postSurvey)
+app.get("/surveys", getSurveys);
+app.get("/surveys/:id", getSurvey);
+app.post("/surveys", postSurvey);
+app.put("/surveys/:id", putSurvey);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
--- src/services/surveys.ts
+++ src/services/surveys.ts
@@ -1,19 +1,36 @@
-import { Survey } from "../../lib/types"
-import { col } from "../app"
+import { ObjectId } from "mongodb";
+import { Survey } from "../../lib/types";
+import { col } from "../app";
 
-export async function postSurveyService(survey : Survey) {
-    const result = await col.insertOne({survey})
-    console.log(result);
-    if (!result) {
-        return new Error("Oh no!")
-    }
-    return result
+export async function postSurveyService(survey: Survey) {
+  const result = await col.insertOne({ survey });
+  if (!result) {
+    return new Error("Oh no!");
+  }
+  return result;
 }
 
-export async function getSurveys() {
-    const result = await col.find({},{"_id":0})
-    if (!result) {
-        return new Error("No surveys found :(")
-    }
-    return result
+export async function getSurveysService() {
+  const result = (await col.find({}, { _id: 0 }).toArray()) as Survey[];
+  if (!result) {
+    return new Error("No surveys found :(");
+  }
+  return result;
+}
+
+export async function getSurveyService(id: string) {
+  const query = { _id: new ObjectId(id) };
+  const result = (await col.findOne(query)) as Survey;
+  if (!result) {
+    return new Error(`No survey found with Id: ${id} :(`);
+  }
+  return result;
+}
+
+export async function putSurveyService(id: string, updatedSurvey: Survey) {
+  const result = await col.updateOne(id, { $set: updatedSurvey });
+  if (!result) {
+    return new Error(`Put request failed :(`);
+  }
+  return result;
 }
