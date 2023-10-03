30083fc69fe620542ee2dcda990d206c62b3a378
Max Chapman <max.chapman@cloudhouse.com>
2023-10-04 00:05:11 +0100

    Finish off basics of API

--- src/controllers/surveys.ts
+++ src/controllers/surveys.ts
@@ -1,10 +1,42 @@
-import { Survey } from "../../lib/types"
-import { postSurveyService } from "../services/surveys"
-import { Request, Response } from "express"
+import { Survey } from "../../lib/types";
+import {
+  getSurveyService,
+  getSurveysService,
+  postSurveyService,
+  putSurveyService,
+} from "../services/surveys";
+import { Request, Response } from "express";
 
-export async function postSurvey(req : Request, res : Response) {
-    const { industry, id, old_salary, new_salary, apprenticeship_grade } : any = req.body
-    const survey = { industry, id, old_salary, new_salary, apprenticeship_grade }
-    await postSurveyService(survey)
-    res.status(201).send("Survey was successfully saved :)")
+export async function postSurvey(req: Request, res: Response) {
+  const { industry, id, old_salary, new_salary, apprenticeship_grade }: Survey =
+    req.body;
+  const survey = { industry, id, old_salary, new_salary, apprenticeship_grade };
+  const result = await postSurveyService(survey);
+  res.status(201).json(result.insertedId);
+}
+
+export async function getSurveys(req: Request, res: Response) {
+  const surveys = await getSurveysService();
+  res.status(200).json(surveys);
+}
+
+export async function getSurvey(req: Request, res: Response) {
+  const id = req.params.id;
+  const survey = await getSurveyService(id);
+  res.status(200).json(survey);
+}
+
+export async function putSurvey(req: Request, res: Response) {
+  const paramId = req.params.id;
+  const { industry, id, old_salary, new_salary, apprenticeship_grade }: Survey =
+    req.body;
+  const newSurvey = {
+    industry,
+    id,
+    old_salary,
+    new_salary,
+    apprenticeship_grade,
+  };
+  const survey = await putSurveyService(paramId, newSurvey);
+  res.status(200).json(survey);
 }
